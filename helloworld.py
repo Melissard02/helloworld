b1535fb9a938cf2138a9ff2ceae626a2a43b01d4
Melissad02 <dic20006@byui.edu>
2025-09-18 15:42:53 -0600

    Hello worlds

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,20 @@
-#Basic of Basics
-print("Hello World")
+import os
+
+#Clear the terminal screen
+os.system('cls')
+
+#Make more visibly interesting
+print("===SUPER COOL HELLO WORLD===")
+
+#Good old hello
+print("Hello World!!!")
+
+print("Does the world say hello back?")
+print("1. Yes")
+print("2. No")
+response = int(input("> "))
+
+if response == 1:
+    print("Hello to you too!!")
+else:
+    print("Oh... :(")
